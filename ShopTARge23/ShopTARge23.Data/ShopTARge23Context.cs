9b1924e961f76bc907e44029598ef3789efb2d91
Opilane TTHK <opilane@tthk.ee>
2024-12-09 10:15:15 +0200

    login

---
+++ ShopTARge23/ShopTARge23.Data/ShopTARge23Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopTARge23.Core.Domain;
+
+
+namespace ShopTARge23.Data
+{
+    public class ShopTARge23Context : IdentityDbContext<ApplicationUser>
+    {
+        public ShopTARge23Context(DbContextOptions<ShopTARge23Context> options) 
+        : base(options) { }
+
+        public DbSet<Spaceship> Spaceships { get; set; }
+        public DbSet<FileToApi> FileToApis { get; set; }
+        public DbSet<RealEstate> RealEstates { get; set; }
+        public DbSet<FileToDatabase> FileToDatabases { get; set; }
+        
+        public DbSet<IdentityRole> IdentityRoles{ get; set; }
+
+
+
+
+    }
+}
---
+++ ShopTARge23/ShopTARge23/Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using ShopTARge23.ApplicationServices.Services;
+using ShopTARge23.Core.Domain;
+using ShopTARge23.Core.ServiceInterface;
+using ShopTARge23.Data;
+
+
+namespace ShopTARge23
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<ISpaceshipsServices, SpaceshipsServices>();
+            builder.Services.AddScoped<IFileServices, FileServices>();
+            builder.Services.AddScoped<IRealEstateServices, RealEstateServices>();
+            builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
+            builder.Services.AddScoped<IChuckNorrisServices, ChuckNorrisServices>();
+            builder.Services.AddScoped<IFreeToGamesServices, FreeToGamesServices>();
+            builder.Services.AddScoped<IFreeToGamesServices, FreeToGamesServices>();
+            builder.Services.AddScoped<ICocktailServices, CocktailServices>();
+            builder.Services.AddScoped<IEmailServices, EmailServices>();
+
+
+            builder.Services.AddDbContext<ShopTARge23Context>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.Password.RequiredLength = 3;
+
+                options.Tokens.EmailConfirmationTokenProvider = "CustomEmailConfirmation";
+                options.Lockout.MaxFailedAccessAttempts = 3;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            })
+            .AddEntityFrameworkStores<ShopTARge23Context>()
+            .AddDefaultTokenProviders()
+            .AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>("CustomEmailConfirmation")
+            .AddDefaultUI();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider
+                (Path.Combine(builder.Environment.ContentRootPath, "multipleFileUpload")),
+                RequestPath = "/multipleFileUpload"
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
